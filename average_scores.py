41e893a90e8e254ed388da2af4d1fbc1290c8bf6
galacticsquad15 <59900338+galacticsquad15@users.noreply.github.com>
2020-02-01 11:08:34 -0600

    Passing
    This is the complete code for average_scores and test_average_scores.

--- average_scores.py
+++ average_scores.py
@@ -1,2 +1,25 @@
+"""
+Creating function to prompt user to enter information and get their average test score
+"""
+def main():
+    first_name = input("Please enter your first name: ")
+    last_name = input("Please enter your last name: ")
+    user_age = input("Please enter your age: ")
+    average_scores = average()
+    print(last_name + ", " + first_name + " Age: " + user_age + " Average Grade: %.2f" % average_scores)
+
 def average():
-    pass
+    score1 = int(input("Enter score 1: "))
+    score2 = int(input("Enter score 2: "))
+    score3 = int(input("Enter score 3: "))
+
+    scores_average = (score1 + score2 + score3) / 3
+
+    return scores_average
+
+if __name__ == '__main__':
+    main()
+
+"""
+See test_average_scores for various input tests.
+"""
--- test_average_scores.py
+++ test_average_scores.py
@@ -6,6 +6,12 @@ class MyTestCase(unittest.TestCase):
     def test_average(self):
         with mock.patch('builtins.input', side_effect=[85, 90, 95]):
             assert average_scores.average() == 90
+    def test_average_zeroes_input(self):
+        with mock.patch('builtins.input', side_effect=[0,0,0]):
+            assert average_scores.average() == 0
+    def test_average_same_input(self):
+        with mock.patch('builtins.input', side_effect=[10,10,10]):
+            assert average_scores.average() == 10
 
 if __name__ == '__main__':
     unittest.main()
