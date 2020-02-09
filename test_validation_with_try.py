db68430e05139d994d9c22a8510dc14cdca7baa8
galacticsquad15 <59900338+galacticsquad15@users.noreply.github.com>
2020-02-09 11:28:17 -0600

    Input validation with try assignment fully passing

--- test_validation_with_try.py
+++ test_validation_with_try.py
@@ -3,9 +3,17 @@ import validation_with_try
 
 
 class MyTestCase(unittest.TestCase):
-    def test_average_negative_input(self):
+    def test_average_negative_input_first_input(self):
         with self.assertRaises(ValueError):
-            validation_with_try.average(-90, 89, 78)
+            validation_with_try.average(-1, 20, 90)
+
+    def test_average_negative_input_second_input(self):
+        with self.assertRaises(ValueError):
+            validation_with_try.average(1, -20, 90)
+
+    def test_average_negative_input_third_input(self):
+        with self.assertRaises(ValueError):
+            validation_with_try.average(1, 2, -3)
 
 
 if __name__ == '__main__':
--- validation_with_try.py
+++ validation_with_try.py
@@ -7,11 +7,31 @@ def main():
     first_name = input("Please enter your first name: ")
     last_name = input("Please enter your last name: ")
     user_age = input("Please enter your age: ")
-    average_scores = average()
+    first_input = float(input("Enter score 1: "))
+
+    while first_input < 0:
+        first_input = float(input("Enter score 1: "))
+
+    second_input = float(input("Enter score 2: "))
+
+    while second_input < 0:
+        second_input = float(input("Enter score 2: "))
+
+    third_input = float(input("Enter score 3: "))
+
+    while third_input < 0:
+        third_input = float(input("Enter score 3: "))
+
+    average_scores = average(first_input, second_input, third_input)
     print(last_name + ", " + first_name + " Age: " + user_age + " Average Grade: %.2f" % average_scores)
 
 
 def average(first_input, second_input, third_input):
+    try:
+        if first_input < 0 or second_input < 0 or third_input < 0:
+            raise ValueError
+    except ValueError:
+        print("Negative number entered for one of the values.")
     scores_average = (first_input + second_input + third_input) / 3
     return scores_average
 
@@ -20,5 +40,5 @@ if __name__ == '__main__':
     main()
 
 """
-See test_average_scores for various input tests.
+See test_validation_with_try for various input tests.
 """
