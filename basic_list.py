595e494965a61244f204a7b82cb329a018dcaf35
galacticsquad15 <59900338+galacticsquad15@users.noreply.github.com>
2020-03-01 18:30:55 -0600

    Basic List Assignment
    Test now passing and main() written.

--- basic_list.py
+++ basic_list.py
@@ -1,12 +1,33 @@
 def main():
-    pass
+    my_list = make_list()
+
+    for i in my_list:
+        print(i)
 
 
 def make_list():
-    pass
+    times = 0
+    the_list = []
+
+    while times < 3:
+        list_input = get_input()
+        the_list.append(float(list_input))
+        times += 1
+
+    return the_list
+
 
 def get_input():
-    pass
+    user_input = input("Enter a number: ")
+
+    while not user_input.isnumeric():
+        user_input = input("Enter a number: ")
+
+    final = float(user_input)
+
+    return final
+
+
 
 
 if __name__ == '__main__':
