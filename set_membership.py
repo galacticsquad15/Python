926df2839051a3ea95ecbeb499623756807a7f2a
galacticsquad15 <59900338+galacticsquad15@users.noreply.github.com>
2020-03-03 18:12:56 -0600

    Set Assignment Tests Failing

---
+++ set_membership.py
@@ -0,0 +1,26 @@
+"""Create a project Module 8 and directories more_fun_with_collections and test.
+
+Create a file test_set_membership.py, a unit test in test.
+Create a file set_membership.py in more_fun_with_collections.
+In set_membership.py
+declare an unimplemented function in_set. This will eventually accept a set and
+return a boolean value stating if the element is in the set. First, it contains pass only.
+In test_set_membership.py,
+Write a test test_in_set_true
+assertion for an item in the set (expect True)
+Write a test test_in_set_false
+One assertion for an item not in the set (expect False)
+Run the test, see it fail, commit to github
+Write the in_set function.
+Run the test, see it pass, commit to github."""
+
+def main():
+    pass
+
+
+def in_set(looking_for, the_set):
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ test_set_membership.py
@@ -0,0 +1,21 @@
+"""
+Write a test test_in_set_true
+assertion for an item in the set (expect True)
+Write a test test_in_set_false
+One assertion for an item not in the set (expect False)
+Run the test, see it fail, commit to github
+Write the in_set function.
+Run the test, see it pass, commit to github.
+"""
+import unittest
+import set_membership
+
+
+class MyTestCase(unittest.TestCase):
+    def test_in_set_false(self):
+        my_set = set{1, 2, 3, 4, 5}
+        self.assertEqual(set_membership.in_set(6, my_set), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
