09b974340e05b7f37b393471e9f00cb5e9510427
galacticsquad15 <59900338+galacticsquad15@users.noreply.github.com>
2020-03-03 18:40:00 -0600

    Basic Dictionary Assignment Tests Passing

--- dict_membership.py
+++ dict_membership.py
@@ -1,25 +1,17 @@
-"""Create a project Module 8 and directories more_fun_with_collections and test.
-
-Create a file test_dict_membership.py, a unit test in test.
-Create a file dict_membership.py in more_fun_with_collections.
-In dict_membership.py
-declare an unimplemented function in_dict. This will eventually
-accept a set and return a boolean value stating if the element is in the set. First, it contains pass only.
-In test_set_membership.py,
-Write a test test_in_dict_true
-assertion for an item in the set (expect True)
-Write a test test_in_dict_false
-One assertion for an item not in the set (expect False)
-Run the test, see it fail, commit to github
-Write the in_dict function.
-Run the test, see it pass, commit to github."""
-
 def main():
     pass
 
 
+#Will be assuming we are just searching for the key.
 def in_dict(looking_for, the_dict):
-    pass
+    list(the_dict)
+
+    if looking_for in the_dict:
+        found = True
+    else:
+        found = False
+
+    return found
 
 
 if __name__ == '__main__':
