a18e9755fbf08f13f76d481acfa0a2863f3ea572
galacticsquad15 <59900338+galacticsquad15@users.noreply.github.com>
2020-03-09 18:56:46 -0500

    Selection Using Dictionary Assignment Tests Passing

--- assign_average.py
+++ assign_average.py
@@ -1,32 +1,20 @@
-"""
-You learned sequence structure, statements executed in order, then selection statements with if statements,
-then looping statements. In some languages, there is a case selection statements, sometimes called switch/case.
-
-In Python, you can write your own case statement using a function and a dictionary.
-
-In this assignment you implement a switch/case with a dictionary.
-
-In Module8 project
-
-Create a file assign_average.py with the unimplemented switch_average() function.
-Create a file test_assign_average.py
-Write a function to test A (and a) return value
-Write functions for the other 4 keys
-Write function to test if a non-key is passed
-What do you expect to happen? Should an exception be raised in the switch? How should you handle the case of no key found?
-Run the tests and see them fail, commit to github
-Begin the implementation of your switch
-Make one test function pass for key value of 'A', then commit to github
-Make the next test function (key of 'B') pass for the next value, then commit to github
-Repeat above step for each test until all are passing.
-"""
-
 def main():
-    pass
+    my_output = switch_average('A')
+    print(my_output)
 
 
-def switch_average(value, dict):
-    pass
+def switch_average(value):
+    the_case = {
+        'A' : 1,
+        'B' : 2,
+        'C' : 3,
+        'D' : 4,
+        'E' : 5,
+    }
+
+    the_output = the_case.get(value, "Invalid entry")
+
+    return the_output
 
 
 if __name__ == '__main__':
--- test_assign_average.py
+++ test_assign_average.py
@@ -4,34 +4,28 @@ import assign_average
 
 class MyTestCase(unittest.TestCase):
     def test_a(self):
-        my_dict = {'A' : 1, 'B' : 2, 'C' : 3, 'D' : 4, 'E' : 5}
         my_value = 'A'
-        self.assertEqual(assign_average.switch_average(my_value, my_dict), True)
+        self.assertEqual(assign_average.switch_average(my_value), 1)
 
     def test_b(self):
-        my_dict = {'A' : 1, 'B' : 2, 'C' : 3, 'D' : 4, 'E' : 5}
         my_value = 'B'
-        self.assertEqual(assign_average.switch_average(my_value, my_dict), True)
+        self.assertEqual(assign_average.switch_average(my_value), 2)
 
     def test_c(self):
-        my_dict = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5}
         my_value = 'C'
-        self.assertEqual(assign_average.switch_average(my_value, my_dict), True)
+        self.assertEqual(assign_average.switch_average(my_value), 3)
 
     def test_d(self):
-        my_dict = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5}
         my_value = 'D'
-        self.assertEqual(assign_average.switch_average(my_value, my_dict), True)
+        self.assertEqual(assign_average.switch_average(my_value), 4)
 
     def test_e(self):
-        my_dict = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5}
         my_value = 'E'
-        self.assertEqual(assign_average.switch_average(my_value, my_dict), True)
+        self.assertEqual(assign_average.switch_average(my_value), 5)
 
-    def incorrect_key(self):
-        my_dict = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5}
+    def test_incorrect_key(self):
         my_value = 'F'
-        self.assertEqual(assign_average.switch_average(my_value, my_dict), False)
+        self.assertEqual(assign_average.switch_average(my_value), "Invalid entry")
 
 
 if __name__ == '__main__':
