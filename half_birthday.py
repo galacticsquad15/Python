ef9590a660133f89ce02bf6c76e7b52948c11f13
galacticsquad15 <59900338+galacticsquad15@users.noreply.github.com>
2020-03-09 19:11:52 -0500

    Datetime Assignment Tests Failing

---
+++ half_birthday.py
@@ -0,0 +1,15 @@
+import datetime
+
+
+def main():
+    pass
+
+
+def my_half_birthday(birthday):
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ test_half_birthday.py
@@ -0,0 +1,13 @@
+import unittest
+import half_birthday
+import datetime
+
+
+class MyTestCase(unittest.TestCase):
+    def test_half_birthday(self):
+        my_birthday = datetime.datetime(1994, 5, 3)
+        self.assertEqual(half_birthday.my_half_birthday(my_birthday), "2020-11-02 00:00:00")
+
+
+if __name__ == '__main__':
+    unittest.main()
